18e0058fb06f903f90a02f94e0f6386eceda1638
xiaoyao-shull <xiaoyao.shull@gmail.com>
2019-12-18 13:29:29 +0800

    MOD: refine union-find

--- Projects/spoj/SOCNETC/SOCNETC.c
+++ Projects/spoj/SOCNETC/SOCNETC.c
@@ -1,67 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_USER_COUNT 100000
+struct uf_t {
+  int * parent;
+  int * weight;
+};
+typedef struct uf_t * uf_t;
 
-int parent[MAX_USER_COUNT + 1];
-int size[MAX_USER_COUNT + 1];
+uf_t uf_create(int size);
+void uf_delete(uf_t * ptr);
+int uf_root(uf_t uf, int i);
+int uf_size(uf_t uf, int i);
+void uf_union(uf_t uf, int i, int j);
 
-void init(int user_count);
-void join(int i, int j, int max_size);
-int find(int i);
-
-void init(int user_count) {
+uf_t uf_create(int size) {
   int i;
-  for (i = 1; i <= user_count; i++) {
-    parent[i] = i;
-    size[i] = 1;
+  struct uf_t * ptr = malloc(sizeof(*ptr) + sizeof(int) * (size + 1) * 2);
+  ptr->parent = (int *) (ptr + 1);
+  ptr->weight = ptr->parent + (size + 1);
+  for (i = 1; i <= size; i++) {
+    ptr->parent[i] = i;
+    ptr->weight[i] = 1;
   }
+  return ptr;
 }
 
-void join(int i, int j, int max_size) {
-  i = find(i);
-  j = find(j);
-  if (size[i] + size[j] > max_size) {
-    return;
-  }
-
-  if (size[i] > size[j]) {
-    parent[j] = i;
-    size[i] += size[j];
-
-  } else {
-    parent[i] = j;
-    size[j] += size[i];
-  }
+void uf_delete(uf_t * ptr) {
+  free(*ptr);
+  *ptr = NULL;
 }
 
-int find(int i) {
-  while (parent[i] != i) {
-    parent[i] = parent[parent[i]];
-    i = parent[i];
+int uf_root(uf_t uf, int i) {
+  while (i != uf->parent[i]) {
+    uf->parent[i] = uf->parent[uf->parent[i]];
+    i = uf->parent[i];
   }
   return i;
 }
 
+int uf_size(uf_t uf, int i) {
+  return uf->weight[uf_root(uf, i)];
+}
+
+void uf_union(uf_t uf, int i, int j) {
+  i = uf_root(uf, i);
+  j = uf_root(uf, j);
+  if (uf_size(uf, i) > uf_size(uf, j)) {
+    uf->parent[j] = i;
+    uf->weight[i] += uf->weight[j];
+
+  } else {
+    uf->parent[i] = j;
+    uf->weight[j] += uf->weight[i];
+  }
+}
+
 int main(int argc, char * argv[]) {
-  int user_count;
-  int max_size;
-  int query_count;
-  int i, j, k;
-  char operation[2];
-  scanf("%d%d%d", &user_count, &max_size, &query_count);
-  init(user_count);
-  for (int i = 0; i < query_count; i++) {
-    scanf("%s", operation);
-    switch(operation[0]) {
+  int n, m, q;
+  int i, j, si, sj;
+  char buffer[2];
+  scanf("%d%d%d", &n, &m, &q);
+  uf_t uf = uf_create(n);
+  while (q--) {
+    scanf("%s", buffer);
+    switch(buffer[0]) {
     case 'A':
-      scanf("%d%d", &j, &k);
-      join(j, k, max_size);
+      scanf("%d%d", &i, &j);
+      si = uf_size(uf, i);
+      sj = uf_size(uf, j);
+      if (si + sj <= m) {
+        uf_union(uf, i, j);
+      }
       break;
 
     case 'E':
-      scanf("%d%d", &j, &k);
-      if (find(j) == find(k)) {
+      scanf("%d%d", &i, &j);
+      if (uf_root(uf, i) == uf_root(uf, j)) {
         printf("Yes\n");
 
       } else {
@@ -70,10 +84,9 @@ int main(int argc, char * argv[]) {
       break;
 
     case 'S':
-      scanf("%d", &j);
-      printf("%d\n", size[find(j)]);
     default:
-      break;
+      scanf("%d", &i);
+      printf("%d\n", uf_size(uf, i));
     }
   }
   return 0;
